e81cd75fccda9778129fcda3062a5969fe007a88
Andrew Park <apark11278@gmail.com>
2018-09-19 16:35:09 -0400

    Updated server.js to include heroku configs

--- server.js
+++ server.js
@@ -23,13 +23,13 @@ app.engine(
 );
 app.set("view engine", "handlebars");
 
-var databaseUri = "mongodb://localhost/mongoscrape";
+// If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 
-if(process.env.MONGODB_URI) {
-    mongoose.connect(process.env.MONGODB_URI);
-} else {
-    mongoose.connect(databaseUri);
-}
+// Set mongoose to leverage built in JavaScript ES6 Promises
+// Connect to the Mongo DB
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI);
 
 htmlRoutes(app);
 apiRoutes(app);
